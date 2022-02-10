050229612c8f0d250b0ab597a2c30b1f4d6fcfc8
GParicio <guillermo.paricio01@gmail.com>
2022-02-10 08:15:54 +0100

    ej1

---
+++ Documents/NetBeansProjects/Segunda/src/Ejercicio01/Programa.java
@@ -0,0 +1,19 @@
+/*
+package Ejercicio01;
+
+public class Programa {
+    public static void main(String arg[]){
+        persona persona1, persona2, persona3;
+
+        persona1 = new Persona();
+        persona2 = new Persona("María", "Dueñas");
+        persona3 = new Persona("Camilo José", "Cela", 1.70, 120);
+
+        persona2 = persona1;
+
+        persona1.mostrar();
+        persona2.mostrar();
+        persona3.mostrar();
+    }
+}
+*/
